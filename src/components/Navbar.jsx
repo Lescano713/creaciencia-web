d35cb374657851c5ac7eed2292864e7b32d3e99b
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2026-02-17 06:27:50 -0500

    adding code to navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav style={{ padding: "1rem", background: "#eee" }}>
+        <h3>Navbar funcionando</h3>
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
-  const { id } = useParams();
+    const { id } = useParams();
 
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h2>Detalle del Producto</h2>
-      <p>ID del producto: {id}</p>
-      <p>Aquí irá la información completa del producto.</p>
-    </div>
-  );
+    return (
+        <div style={{ padding: '2rem' }}>
+        <h2>Detalle del Producto</h2>
+        <p>ID del producto: {id}</p>
+        <p>Aquí irá la información completa del producto.</p>
+        </div>
+    );
 };
 
 export default ProductDetail;
