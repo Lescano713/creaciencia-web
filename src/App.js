ec9dad515bf61325d5bf59ab2c2fdd0dd34c0c8e
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2026-02-17 05:41:43 -0500

    test minimal app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Admin from './pages/Admin';
-import ProductDetail from './pages/ProductDetail';
-import Navbar from './components/Navbar';
-
 function App() {
   return (
-    <Router>
-      <Navbar /> {/* El Navbar se verá en todas las páginas */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin-creaciencia" element={<Admin />} />
-        <Route path="/producto/:id" element={<ProductDetail />} />
-      </Routes>
-    </Router>
+    <div style={{ padding: "2rem" }}>
+      <h1>App funcionando</h1>
+    </div>
   );
 }
+
 export default App;
