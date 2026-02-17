f5180341639fa09d56ff27c8ef4fb2a1a122901d
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2026-02-17 06:15:43 -0500

    running app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-function App() {
+/*function App() {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>App funcionando</h1>
     </div>
   );
-}
+}*/
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import ProductDetail from './pages/ProductDetail';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <h1>MIAU</h1>
+      <Navbar /> {/* El Navbar se verá en todas las páginas */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin-creaciencia" element={<Admin />} />
+        <Route path="/producto/:id" element={<ProductDetail />} />
+      </Routes>
+    </Router>
+  );
+}
 export default App;
+
