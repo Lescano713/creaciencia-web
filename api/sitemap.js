c1519d8d88742015e568c66d576f1d6d04ff4d3b
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2026-02-22 03:49:24 -0500

    fixing sitemap error

--- api/sitemap.js
+++ api/sitemap.js
@@ -14,6 +14,7 @@ if (!getApps().length) {
 const db = getFirestore();
 
 export default async function handler(req, res) {
+
   try {
 
     const snapshot = await getDocs(collection(db, "productos"));
@@ -29,39 +30,19 @@ export default async function handler(req, res) {
       `;
     }).join("");
 
-    const staticPages = `
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>https://www.creacienciaperu.com/</loc>
-        <changefreq>weekly</changefreq>
         <priority>1.0</priority>
       </url>
-      <url>
-        <loc>https://www.creacienciaperu.com/productos</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.9</priority>
-      </url>
-      <url>
-        <loc>https://www.creacienciaperu.com/contacto</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.7</priority>
-      </url>
-      <url>
-        <loc>https://www.creacienciaperu.com/nosotros</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.7</priority>
-      </url>
-    `;
-
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${staticPages}
-        ${productos}
-      </urlset>`;
+      ${productos}
+    </urlset>`;
 
     res.setHeader("Content-Type", "text/xml");
     res.status(200).send(sitemap);
 
   } catch (error) {
-    res.status(500).json({ error: "Error generating sitemap" });
+    res.status(500).send("Error generating sitemap");
   }
 }
